7091d2023270870150303c25c6f6df2f7e0b6c36
Ryan Pennucci <decimalman@gmail.com>
2012-08-25 08:45:28 -0400

    64-bit support in build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 echo "Building frfix.so..."
-if gcc -Wall -fPIC -shared -m32 -Os -s `pkg-config --cflags alsa` -o frfix.so frfix.c "$@"
+if file [Ff]ieldrunners 2>/dev/null | grep -q '64-bit'
+then cflags=-m64; src=frfix-64.so.prebuilt
+else cflags=-m32; src=frfix-32.so.prebuilt
+fi
+if gcc -Wall -fPIC -shared -Os -s `pkg-config --cflags alsa` $cflags -o frfix.so frfix.c "$@"
 then	echo "Done."
 else	echo "Failed.  Falling back to prebuilt frfix.so..."
-	mv frfix.so.prebuilt frfix.so || \
+	mv $src frfix.so || \
 		echo "Failed.  Do you have permissions to write to the current directory?"
 fi
