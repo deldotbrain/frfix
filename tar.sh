1f03d4dd20f1c502e342d4e3ac4b373beaa0ca5c
Ryan Pennucci <decimalman@gmail.com>
2013-10-01 18:14:24 -0400

    Updates for public release
    - Rename README for GitHub
    - Add tar.sh for packaging
    - Remove unused alsa.conf

---
+++ tar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#./build.sh -o 'frfix.so.prebuilt'
+echo "building 32- and 64-bit libraries"
+gcc -Wall -fPIC -shared -Os -m32 -s `pkg-config --cflags alsa` $cflags -o frfix-32.so.prebuilt frfix.c "$@"
+gcc -Wall -fPIC -shared -Os -m64 -s `pkg-config --cflags alsa` $cflags -o frfix-64.so.prebuilt frfix.c "$@"
+echo "tarring everything up..."
+tar c build.sh frfix.c frfix-32.so.prebuilt frfix-64.so.prebuilt launch.sh | bzip2 -9 > frfix-$(date +%F).tar.bz2
+echo "done"
